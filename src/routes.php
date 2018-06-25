d9a4cd231f0fa5ec0443e9263c5c497bb64f6a4e
Hemant Kumar Sah <hemantanshu@gmail.com>
2018-06-26 00:33:02 +0200

    fixed user session details

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 Route::group(['namespace' => 'Drivezy\LaravelAccessManager\Controllers'], function () {
 
-    Route::get('getSessionDetails', 'LoginController@getLoginSession');
+    Route::get('getSessionDetails', 'LoginController@getSessionDetails');
 });
 
 
