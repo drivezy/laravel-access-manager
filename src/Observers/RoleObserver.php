c8493c6a5bb14ac7e031175d02cf472b67867e14
Hemant Kumar Sah <hemantanshu@gmail.com>
2020-03-09 15:12:09 +0530

    added extra logic on the role observer

--- src/Observers/RoleObserver.php
+++ src/Observers/RoleObserver.php
@@ -2,11 +2,30 @@
 
 namespace Drivezy\LaravelAccessManager\Observers;
 
+use Drivezy\LaravelAccessManager\Models\RoleAssignment;
 use Drivezy\LaravelUtility\Observers\BaseObserver;
+use Illuminate\Database\Eloquent\Model as Eloquent;
 
-class RoleObserver extends BaseObserver {
+/**
+ * Class RoleObserver
+ * @package Drivezy\LaravelAccessManager\Observers
+ */
+class RoleObserver extends BaseObserver
+{
+    /**
+     * @var array
+     */
     protected $rules = [
         'name'       => 'required',
         'identifier' => 'required',
     ];
+
+    /**
+     * @param Eloquent $model
+     */
+    public function deleted (Eloquent $model)
+    {
+        RoleAssignment::where('role_id', $model->id)->delete();
+        parent::deleted($model);
+    }
 }
